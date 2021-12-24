6bdc703634ba8dd513fe8917c3db9ccc9d5b7940
Tom Bowyer <tom4310@gmail.com>
2021-12-24 09:19:43 +0000

    Deploy method

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Building new content...'
+rm -rf public
+hugo
+
+echo 'Moving new content'
+rm -rf serve/public
+mv public serve/public
+
+echo 'Recreating docker container'
+docker-compose -f serve/docker-compose.yml up --force-recreate -d
